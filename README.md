5f7133f3cfdc66ad3bef6a099925e38e5da12b02
kielsonzinn <kielsonzinn@gmail.com>
2023-07-01 16:01:51 -0300

    create extension acr-assigned

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# acr-extension-name
+# acr-assigned
+
+Extensão que verifica se o merge request possui um assigned
 
 Arquivo config.json
 
 ```json
 {
-
 }
 ```
--- src/review.py
+++ src/review.py
@@ -1,24 +1,12 @@
 def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
-
     merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  {
-    #      "id": "",
-    #      "comment": "",
-    #      "position": {
-    #          "language": "",
-    #          "path': "",
-    #          "startInLine": 0,
-    #          "endInLine": 0
-    #    }
-    #  }
+
+    if 'assignee' not in merge or merge['assignee'] is None:
+        comments.append({
+            "id": "1",
+            "comment": "Assignned não atribuído"
+        })
 
     return comments
