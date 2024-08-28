78cff2008f2b932e078dd40a45c2fb2a0314d2d6
Kielson Zinn da Silva <kielsonzinn@gmail.com>
2024-08-28 19:06:10 -0300

    Adiciona funcionalidade denyList e allowList

--- src/review.py
+++ src/review.py
@@ -4,9 +4,39 @@ def review(config):
     comments = []
 
     if 'assignee' not in merge or merge['assignee'] is None:
+        if 'message' in config:
+            message = config['message']
+        else:
+            message = "Assignned não atribuído"
+
         comments.append({
-            "id": "1",
-            "comment": "Assignned não atribuído"
+            "id": "NOT_ASSIGNED",
+            "comment": message
         })
+    else:
+        author = merge['author']
+        assigned_to = merge['assignee']
+
+        if 'allowList' in config:
+            allow_list = config['allowList']
+            allow_list = [username.replace("${AUTHOR_MERGE}", author) for username in allow_list]
+
+            if assigned_to not in allow_list:
+                message = config['allowMessage'].replace("${USERS}", ", ".join(allow_list))
+                comments.append({
+                    "id": "ALLOW_LIST",
+                    "comment": message
+                })
+
+        if 'denyList' in config:
+            deny_list = config['denyList']
+            deny_list = [username.replace("${AUTHOR_MERGE}", author) for username in deny_list]
+
+            if assigned_to in deny_list:
+                message = config['denyMessage'].replace("${USERS}", ", ".join(deny_list))
+                comments.append({
+                    "id": "DENY_LIST",
+                    "comment": message
+                })
 
     return comments
